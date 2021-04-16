593a68aed27619d561e54976c84593346e0512f8
Guilhermeartt <44584941+Guilhermeartt@users.noreply.github.com>
2021-04-16 19:56:07 -0300

    Tratamento de respostas as interações do usuário

--- cursos/Format.py
+++ cursos/Format.py
@@ -34,7 +34,7 @@ while True:
 
 
         ler = data_file.readable()
-        print (ler)
+        # print (ler)
 
         lines = data_file.readlines()
 
@@ -44,7 +44,7 @@ while True:
         q = name_file.split('/')
         q.pop()
         p ="/".join(q)
-        #print (p)
+        # print (p)
 
         # Coloca o endereco do arquivo como diretorio destino
         os.chdir( p )
@@ -52,38 +52,47 @@ while True:
         # Separa o endeco do arquivo para adicionar o prefixo "RES_"
         x = name_file.split('/')
         x.reverse()
-        # print (x)
+        #
+        print (x[0])
 
+        name_file = x[0]
 
-        data_file = open("RES_" + x[0], 'w+',encoding="utf-8")
-        for line in lines:
-            # Passo 1
-            if 'td' in line:
-                line = re.sub('width="([0-9%]*)" ', '', line)
-            # Passo 2
-            if 'style=' in line:
-                line = re.sub('width:([a-z0-9\.%]*;)', '', line)
-            # Passo 3
-            if 'style=' in line:
-                line = re.sub('height:([a-z0-9\.%]*;)', '', line)
-            # Passo 4
-            if 'class=' in line:
-                line = re.sub('CxSpFirst', '', line)
-            # Passo 5
-            if 'class=' in line:
-                line = re.sub('CxSpLast', '', line)
-            # Passo 6
-            if 'class=' in line:
-                line = re.sub('CxSpMiddle', '', line)
-            # Passo 7
-            if '<' in line:
-                line = re.sub('<!--', '', line)
-            if '>' in line:
-                line = re.sub('-->', '', line)
+        f = name_file.split('.')
+        # print (f)
+        if f[1] == 'html':
+            print ('\n' + 'extenção valida!' + '\n')
 
-            data_file.write(line)
+            data_file = open("RES_" + x[0], 'w+',encoding="utf-8")
+            for line in lines:
+                # Passo 1
+                if 'td' in line:
+                    line = re.sub('width="([0-9%]*)" ', '', line)
+                # Passo 2
+                if 'style=' in line:
+                    line = re.sub('width:([a-z0-9\.%]*;)', '', line)
+                # Passo 3
+                if 'style=' in line:
+                    line = re.sub('height:([a-z0-9\.%]*;)', '', line)
+                # Passo 4
+                if 'class=' in line:
+                    line = re.sub('CxSpFirst', '', line)
+                # Passo 5
+                if 'class=' in line:
+                    line = re.sub('CxSpLast', '', line)
+                # Passo 6
+                if 'class=' in line:
+                    line = re.sub('CxSpMiddle', '', line)
+                # Passo 7
+                if '<' in line:
+                    line = re.sub('<!--', '', line)
+                if '>' in line:
+                    line = re.sub('-->', '', line)
 
-        data_file.close()
+                data_file.write(line)
 
-        print('\n' + 'Arquivo alterado com sucesso!' + '\n')
+            data_file.close()
+
+            print ('\n' + 'Arquivo alterado com sucesso!' + '\n')
+        else:
+            print ('\n' + 'extenção invalida, selecione um arquivo "html"!' + '\n')
         break
