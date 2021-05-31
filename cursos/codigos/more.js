5151af10556e169d3c3471ce4050c5fb44c0c65c
Guilhermeartt <44584941+Guilhermeartt@users.noreply.github.com>
2021-05-31 03:03:37 -0300

    Update more.js

--- cursos/codigos/more.js
+++ cursos/codigos/more.js
@@ -2,15 +2,15 @@
 // @alvesitalo
 
 // Hide all
-$('.tabelaneuro tbody').hide();
+//$('.tabelaneuro tbody').hide();
 
 // Button behavior
-$('.toggle-more').click(function() {
-  const table_id = '#' + $(this).attr('target') + ' tbody'
+//$('.toggle-more').click(function() {
+////  const table_id = '#' + $(this).attr('target') + ' tbody'
 
-  if ($(table_id).is(':hidden')) {
-    $(table_id).show(500);
-  } else {
-    $(table_id).hide(500);
-  }
-});
+//  if ($(table_id).is(':hidden')) {
+//    $(table_id).show(500);
+//  } else {
+///    $(table_id).hide(500);
+//  }
+//});
