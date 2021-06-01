545801173670337b337bb5dcac0e56e57d11009d
Guilhermeartt <44584941+Guilhermeartt@users.noreply.github.com>
2021-06-01 18:23:51 -0300

    Update more.js

--- cursos/codigos/more.js
+++ cursos/codigos/more.js
@@ -1,21 +1,4 @@
-// Saiba Mais buttons
-// @alvesitalo
-
-// Hide all
-//$('.tabelaneuro tbody').hide();
-
-// Button behavior
-//$('.toggle-more').click(function() {
-////  const table_id = '#' + $(this).attr('target') + ' tbody'
-
-//  if ($(table_id).is(':hidden')) {
-//    $(table_id).show(500);
-//  } else {
-///    $(table_id).hide(500);
-//  }
-//});
-
-var x = document.getElementById("aula-titulo-0").parentElement.style.visibility = "hidden";
+document.getElementById("aula-titulo-0").parentElement.style.visibility = "hidden";
 
 var el = document.getElementsByClassName("tabelaneuro");
 
