a55b88815564755f72c302cae6b1c6f207ae0f94
Ítalo Alves <italooko@gmail.com>
2021-06-11 15:49:44 -0300

    Adiciona show/hide das referências

--- cursos/codigos/more.js
+++ cursos/codigos/more.js
@@ -12,11 +12,11 @@ function configQuadro() {
     hideTd.setAttribute('class', 'hide');
 
     button.setAttribute('target', 'hide-' + z);
-    button.addEventListener('click', window.onload = trocar);
+    button.addEventListener('click', window.onload = trocaQuadro);
   }
 }
 
-function trocar(event) {
+function trocaQuadro(event) {
   var button = event.target.closest('td')
 
   var id = button.getAttribute('target')
@@ -32,7 +32,7 @@ function trocar(event) {
 function destaqueQuadro() {
   var table = document.getElementsByClassName("tabelaneuro");
 
-  for(i = 0; i < table.length; i++) {
+  for(var i = 0; i < table.length; i++) {
     var quadro = table[i];
     quadro.setAttribute("id", "tabQuadro-" + i)
 
@@ -41,13 +41,37 @@ function destaqueQuadro() {
   }
 }
 
-function hideReferencia() {
-  var u = document.getElementsByClassName("Ptabela");
-  u[2].style.display = "none";
+function configReferencia() {
+  var tables = document.getElementsByClassName("Ptabela");
+  var references = tables[2];
+
+  if (references) {
+    references.addEventListener('click', window.onload = trocaReferencia);
+    var trs = references.rows;
+
+    for(var i = 1; i < trs.length; i++) {
+      trs[i].classList.add("hide-tr");
+    }
+  }
+}
+
+function trocaReferencia() {
+  var references = document.getElementsByClassName("Ptabela")[2];
+  var trs = references.rows;
+
+  if (trs) {
+    for(var i = 1; i < trs.length; i++) {
+      if (trs[i].classList.contains('hide-tr')) {
+        trs[i].classList.remove('hide-tr');
+      } else {
+        trs[i].classList.add("hide-tr");
+      }
+    }
+  }
 }
 
 window.onload = function init() {
-  hideReferencia();
   destaqueQuadro();
-  configQuadro(); 
+  configQuadro();
+  configReferencia();
 }
