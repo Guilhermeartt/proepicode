c8418eabdb2e1628cc3725eb18308b9a54b14b8f
Ítalo Alves <italooko@gmail.com>
2021-07-13 13:33:08 -0300

    Adiciona moodle.js

---
+++ cursos/codigos/moodle.js
@@ -0,0 +1,24 @@
+// Auxiliary moodle script
+
+// Change Questionary navigation
+$(document).ready(function() {
+    var container = $('#page-mod-quiz-view .activity-navigation');
+
+    var courseA = $('.breadcrumb-item a');
+    var courseLink = courseA[1].getAttribute('href');
+    var courseID = courseLink.split('=')[1];
+
+    var continuebutton = '<div class="continuebutton"></div>';
+    var form = '<form method="get" action="https://ead.proepi.org.br/ead/course/view.php"></form>';
+    var input = '<input type="hidden" name="id" value="' + courseID + '"></input>';
+    var submit = '<button type="submit" class="btn btn-secondary">Voltar ao curso</button>';
+
+    container.html(continuebutton);
+
+    var newContainer = container.find('div');
+    newContainer.append(form);
+
+    var newForm = newContainer.find('form');
+    newForm.append(input);
+    newForm.append(submit);
+})
